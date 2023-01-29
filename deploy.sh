aeaebe749aa7c00c945d9e1f0a75e37c6e0e4885
David Dias <david6dias@gmail.com>
2023-01-29 18:48:37 +0000

    Add quiet amend

--- deploy.sh
+++ deploy.sh
@@ -21,6 +21,7 @@ git config --global alias.ci commit
 git config --global alias.co checkout
 git config --global alias.st status
 git config --global alias.tree "log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(bold cyan)%aD%C(reset) %C(bold green)(%ar)%C(reset)%C(bold yellow)%d%C(reset)%n''          %C(white)%s%C(reset) %C(dim white)- %an%C(reset)' --all"
+git config --global alias.qa "commit --amend --no-edit"  # quiet amend
 
 # git configs
 git config --global core.editor "vim"
